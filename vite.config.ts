acaa5c3c430a096ee5c2457bc5fcd3429c9a5566
Wuxh <wxh1220@gmail.com>
2021-12-20 10:49:48 +0800

    chore: 配置 alias

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,21 @@
+import path from 'path'
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import styleImport, { AntdResolve } from 'vite-plugin-style-import'
 
+const resolveCwd = (...arg) => path.resolve(process.cwd(), ...arg)
+
 // https://vitejs.dev/config/
 export default defineConfig({
+  resolve: {
+    alias: {
+      '@': resolveCwd('./src'),
+      '@components': resolveCwd('./src/shared/components'),
+      '@const': resolveCwd('./src/shared/const'),
+      '@hooks': resolveCwd('./src/shared/hooks'),
+      '@utils': resolveCwd('./src/shared/utils'),
+    },
+  },
   css: {
     preprocessorOptions: {
       less: {
