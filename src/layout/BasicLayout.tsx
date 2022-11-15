5db9c01fc801c3cbfacafa58c80bd5286264564c
wuxh <wxh1220@gmail.com>
2022-11-15 10:57:37 +0800

    feat: add emotion demo

--- src/layout/BasicLayout.tsx
+++ src/layout/BasicLayout.tsx
@@ -1,11 +1,14 @@
 import React from 'react'
 import ErrorBoundary from '@components/ErrorBoundary'
 import ModalContainer from '@/shared/components/ModalContainer'
+import Emotion from '@/shared/components/Emotion'
 
 const BasicLayout: React.FC = ({ children }) => {
   return (
     <>
-      <p>header</p>
+      <header>
+        <Emotion />
+      </header>
       <hr />
       <ErrorBoundary>
         <ModalContainer />
---
+++ src/shared/components/Emotion/index.tsx
@@ -0,0 +1,15 @@
+import { css } from '@emotion/react'
+
+const Index = () => {
+  return (
+    <div
+      css={css`
+        color: green;
+      `}
+    >
+      hello emotion
+    </div>
+  )
+}
+
+export default Index
