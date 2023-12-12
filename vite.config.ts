853caf1c3edaf846c94620f11fac875d538f41dc
wuxh <wxh1220@gmail.com>
2023-12-12 23:00:48 +0800

    chore: remove vite plugin

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,6 @@ import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 import styleImport, { AntdResolve } from 'vite-plugin-style-import'
 import WindiCSS from 'vite-plugin-windicss'
-import vitePluginIntegrationIntlMessage from './build/vitePluginIntegrationIntlMessage'
 
 const resolveCwd = (...arg) => path.resolve(process.cwd(), ...arg)
 
@@ -33,7 +32,6 @@ export default defineConfig(({ mode }) => ({
     host: '0.0.0.0',
   },
   plugins: [
-    vitePluginIntegrationIntlMessage(),
     react({
       jsxImportSource: '@emotion/react',
       babel: {
