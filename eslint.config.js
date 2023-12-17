d5e829b3b651c31ac7cc735b24b6320d154a9b1d
wuxh <wxh1220@gmail.com>
2023-12-17 15:18:42 +0800

    chore: update react eslint deps

--- eslint.config.js
+++ eslint.config.js
@@ -1,5 +1,20 @@
-import antfu from '@antfu/eslint-config'
+import antfu from "@antfu/eslint-config";
 
-export default antfu({
-  react: true,
-})
+export default antfu(
+  {
+    react: true,
+  },
+  {
+    files: ["**/*.tsx", "**/*.jsx"],
+    rules: {
+      "react/no-unknown-property": [
+        "error",
+        {
+          "ignore": [
+            "css"
+          ]
+        }
+      ]
+    },
+  }
+);
