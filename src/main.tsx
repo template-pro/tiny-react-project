5aec273f7e1aa3d777a326970d0d918a465016f2
红 <wxh1220@gmail.com>
2024-01-02 16:17:24 +0800

    build: fix error (#8)

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,16 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import pkg from '../package.json'
 import Root from '@/root/Root'
 import RootRouter from '@/root/RootRouter'
 
 import 'virtual:windi.css'
+
+import routes from '~react-pages'
+
+window.console.log(`%c${pkg.name.toLocaleUpperCase()} => main.tsx`, 'color:#0B7EA4;', {
+  routes,
+})
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
